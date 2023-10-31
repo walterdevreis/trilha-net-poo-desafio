4b7495dcc9c483171f17197669571ad9d5e93e73
walter dev reis <walter-reis@outlook.com>
2023-10-31 15:53:05 -0300

    Conclusão do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "Nokia 5530 Xpress Music", imei: "456123", memoria: 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("YouTube Music");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "9874", modelo: "iPhone 11", imei: "654789", memoria: 1024);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Nubank");
