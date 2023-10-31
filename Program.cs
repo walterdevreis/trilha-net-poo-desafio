adfe583f7d6c32c0568e33b01dbbd8269a8f2efc
walter dev reis <walter-reis@outlook.com>
2023-10-31 15:59:29 -0300

    Melhorando a saída no console

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Console.WriteLine("Smartphone Nokia:");
+Console.WriteLine("Marca: Nokia.");
 Smartphone nokia = new Nokia(numero: "1234", modelo: "Nokia 5530 Xpress Music", imei: "456123", memoria: 512);
 nokia.Ligar();
 nokia.InstalarAplicativo("YouTube Music");
 
 Console.WriteLine("\n");
 
-Console.WriteLine("Smartphone iPhone:");
+Console.WriteLine("Marca: iPhone.");
 Smartphone iphone = new Iphone(numero: "9874", modelo: "iPhone 11", imei: "654789", memoria: 1024);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Nubank");
